e9de123c688fcc9fdbc880f2e84a676ddaf4f9cd
Alexandre Dionizio <aleactis@hotmail.com>
2019-11-06 17:35:07 -0300

    testes

--- Game_OO/Item.cs
+++ Game_OO/Item.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace Game_OO
 {
